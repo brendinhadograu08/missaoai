751f6f0fb02b2980cb443be317e6f3fcfee9a57a
brendinhadograu08 <129185529+brendinhadograu08@users.noreply.github.com>
2024-08-27 08:29:27 -0300

    ththh

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const caixaPrincipal =document.querySelector(".caixa-principal");
 const caixaPergunta =document.querySelector(".caixa-pergunta");
-const caixaAlternadas =document.querySelector
+const caixaAlternadas =document.querySelector(".caixa-alternativas");
+const caixaResultado =document.querySelector(".caixa-resultado");
+const textoResultado =document.querySelector(".texto-resultado");
