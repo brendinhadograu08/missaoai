ae237ff7a99f4f268cd39277654bad3bcb0d1a4c
brendinhadograu08 <129185529+brendinhadograu08@users.noreply.github.com>
2024-08-27 08:38:06 -0300

    bvxcbxc

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const caixaPergunta =document.querySelector(".caixa-pergunta");
 const caixaAlternadas =document.querySelector(".caixa-alternativas");
 const caixaResultado =document.querySelector(".caixa-resultado");
 const textoResultado =document.querySelector(".texto-resultado");
+
+let exemplo = "uma questão de pergunta";
+let exemplo2 = 23;
+let exemplo3 = false;
+let exemplo4 = true;
+
+let exemplolista = [" frase1,"]
